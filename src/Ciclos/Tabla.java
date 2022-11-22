bd0e1d8ca584a3a5a15d283fc079f5677adfedef
Melinatexeira <117683416+Melinatexeira@users.noreply.github.com>
2022-11-22 10:14:49 -0300

    Update Tabla.java

--- src/Ciclos/Tabla.java
+++ src/Ciclos/Tabla.java
@@ -1,2 +1,17 @@
-package Ciclos;public class Tabla {
+package Ciclos;
+public class Tabla {
+   public static void main(String[] args) {
+
+        Scanner entradaEsc = new Scanner(System.in);
+
+        System.out.println("Introduce un número:");
+        int valor1 = entradaEsc.nextInt();
+
+        for(int valor2=1;valor2<=10;valor2++) {
+            int resultado = valor1 * valor2;
+            System.out.println(valor1 + " x " +valor2 + " = " + resultado);
+
+        }
+
+    }
 }
